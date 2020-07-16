5ff36df8315288c79fa1c7d08fccb4097c8d4974
mirhamasala <mirha.masala@me.com>
2020-07-17 07:42:23 +0800

    Fix typos

--- src/components/ShareNetwork.vue
+++ src/components/ShareNetwork.vue
@@ -11,8 +11,8 @@
         the world during this COVID-19 pandemic.
         Travel safe!"
       quote="Get the latest travel reopening information"
-      hashtags="touherotravel"
-      twitterUser="touherotravel"
+      hashtags="tourherotravel"
+      twitterUser="tourherotravel"
     >
       <img :src="require(`@/assets/images/social_${network.icon}.svg`)"
         class="h-6 w-6 mr-2 sm:mr-0 sm:ml-3 cursor-pointer" />
