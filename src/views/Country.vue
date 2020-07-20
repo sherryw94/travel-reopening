289b1183af214f196184aeafc8e66f9b93246e30
mirhamasala <mirha.masala@me.com>
2020-07-20 15:29:58 +0800

    Add id for country subscribe link

--- src/views/Country.vue
+++ src/views/Country.vue
@@ -11,7 +11,8 @@
           <p class="inline-flex mr-1 mb-0 font-semibold text-sm">
             Want the latest travel updates in your inbox?
           </p>
-          <router-link :to="{name: 'Subscribe'}" class="inline-flex font-semibold text-sm">
+          <router-link :to="{name: 'Subscribe'}" class="inline-flex font-semibold text-sm"
+            id="country-subscribe">
             Subscribe here →
           </router-link>
         </div>
