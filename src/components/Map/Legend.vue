f670480a5f299a13d14a8dc99c55a24186fd1886
Yuri Kovalov <yuri@yurikoval.com>
2020-07-30 13:45:19 +0800

    Clean up Legend

--- src/components/Map/Legend.vue
+++ src/components/Map/Legend.vue
@@ -1,23 +1,27 @@
 <template>
   <div class="inline-flex md:flex-col flex-wrap">
-    <TravelStateBadge travelState="selected"
-      class="mr-1 md:mr-0 text-xs md:text-sm mb-1"/>
-    <TravelStateBadge travelState="yes"
-      class="mr-1 md:mr-0 text-xs md:text-sm mb-1"/>
-    <TravelStateBadge travelState="partial"
-      class="mr-1 text-xs md:mr-0 md:text-sm mb-1"/>
-    <TravelStateBadge travelState="no"
-      class="mr-1 md:mr-0 text-xs md:text-sm mb-1"/>
-    <TravelStateBadge travelState="unknown"
-      class="md:mb-0 mr-1 md:mr-0 text-xs md:text-sm mb-1"/>
+    <TravelStateBadge v-for="badge in badges" :key="badge" :travelState="badge"
+      class="mr-1 md:mr-0 text-xs md:text-sm mb-1" />
   </div>
 </template>
 
 <script>
 import TravelStateBadge from '@/components/TravelStateBadge.vue';
+import { OpenStatus } from '@/constants/travel';
 
 export default {
   name: 'Legend',
   components: { TravelStateBadge },
+  data() {
+    return {
+      badges: [
+        'selected',
+        OpenStatus.Open,
+        OpenStatus.Partial,
+        OpenStatus.Closed,
+        'unknown',
+      ],
+    };
+  },
 };
 </script>
