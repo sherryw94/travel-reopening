0f977b5df1af669ba7130662c1bf8ccc7c9e2d35
mirhamasala <mirha.masala@me.com>
2020-08-04 14:46:07 +0800

    Create CountryFlag component

---
+++ src/components/CountryFlag.vue
@@ -0,0 +1,17 @@
+<template>
+  <span :class="countryFlag"></span>
+</template>
+
+<script>
+export default {
+  name: 'CountryFlag',
+  computed: {
+    countryFlag() {
+      return `flag-icon flag-icon-${this.countryCode.toLowerCase()} shadow`;
+    },
+  },
+  props: {
+    countryCode: String,
+  },
+};
+</script>
