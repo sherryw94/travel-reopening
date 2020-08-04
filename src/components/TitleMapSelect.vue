cb5e3db23d789661e053abbf892e3b3b23323dd0
Yuri Kovalov <yuri@yurikoval.com>
2020-08-04 17:17:15 +0800

    Remove nofollow from tourhero urls

--- src/components/TitleMapSelect.vue
+++ src/components/TitleMapSelect.vue
@@ -8,7 +8,7 @@
             <div class="sm:order-last mt-1 mb-2 sm:my-0"><ShareNetwork /></div>
             <p class="w-full md:w-9/12 sm:order-first">
               A free and open-source project by
-              <a class="inline-flex no-underline" href="https://www.tourhero.com/" target="_blank" rel="nofollow">TourHero</a>.
+              <a class="inline-flex no-underline" href="https://www.tourhero.com/" target="_blank">TourHero</a>.
               <span class="inline-block mb-3 sm:mb-0">Made with
                 <svg class="inline h-6 w-6 text-secondary fill-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100"><path d="M33.78 18C22.142 18 15 28.178 15 36.526 15 54.578 35.517 71.333 50 82c14.483-10.667 35-27.422 35-45.474C85 28.178 77.858 18 66.22 18c-6.5 0-12.053 5.182-16.22 10.105C45.833 23.182 40.28 18 33.78 18z"/></svg>
                 by travelers for travelers.
