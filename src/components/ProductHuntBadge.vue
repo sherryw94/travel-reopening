a0ca914601a448cc11fe19aadea2a2567f42b1f8
mirhamasala <mirha.masala@me.com>
2020-08-11 12:29:59 +0800

    Create ProductHuntBadge component

---
+++ src/components/ProductHuntBadge.vue
@@ -0,0 +1,12 @@
+<template>
+  <a class="block hover:opacity-100" href="https://www.producthunt.com/?utm_source=badge-featured&utm_medium=badge" target="_blank" rel="nofollow noopener">
+    <img class="h-10 w-auto" src="https://api.producthunt.com/widgets/embed-image/v1/featured.svg?post_id=140788&theme=light" alt="Product Hunt Embed"/>
+  </a>
+</template>
+
+<script>
+export default {
+  name: 'ProductHuntBadge',
+};
+
+</script>
