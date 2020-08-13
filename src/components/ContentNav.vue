606c6e96ee134c4947006fdc5c3e06968b2df320
mirhamasala <mirha.masala@me.com>
2020-08-13 15:59:20 +0800

    Add IDs to content nav links

--- src/components/ContentNav.vue
+++ src/components/ContentNav.vue
@@ -4,27 +4,25 @@
       <div class="main-container mb-0">
         <div class="main-container__inner ">
           <nav class="flex -mb-px">
-            <router-link
-              :to="{hash: '#map'}" class="py-3 pr-1
+            <router-link :to="{hash: '#map'}" id="sitenav2-map" class="py-3 pr-1
               text-gray-700 no-underline border-b-2 border-transparent
               hover:opacity-100 sm:hover:border-gray-300
               focus:text-tertiary whitespace-no-wrap sm:focus:border-tertiary">
               Map
             </router-link>
-            <router-link
-              :to="{hash: '#international-travel'}" class="py-3 px-1 ml-4 sm:ml-8
+            <router-link :to="{hash: '#international-travel'}" id="sitenav2-international" class="py-3 px-1 ml-4 sm:ml-8
               text-gray-700 no-underline border-b-2 border-transparent
               hover:opacity-100 sm:hover:border-gray-300
               focus:text-tertiary whitespace-no-wrap sm:focus:border-tertiary">
               International Travel
             </router-link>
-            <router-link :to="{hash: '#visa-quarantine-measures'}" class="py-3 px-1 ml-4 sm:ml-8
+            <router-link :to="{hash: '#visa-quarantine-measures'}" id="sitenav2-visa-quarantine" class="py-3 px-1 ml-4 sm:ml-8
               text-gray-700 no-underline border-b-2 border-transparent
               hover:opacity-100 sm:hover:border-gray-300
               focus:text-tertiary whitespace-no-wrap sm:focus:border-tertiary">
               Visa &amp; Quarantine Measures
             </router-link>
-            <router-link :to="{hash: '#domestic-travel'}" class="py-3 pl-1 pr-4 sm:px-1
+            <router-link :to="{hash: '#domestic-travel'}" id="sitenav2-domestic" class="py-3 pl-1 pr-4 sm:px-1
               ml-4 sm:ml-8 text-gray-700 no-underline border-b-2 border-transparent
               hover:opacity-100 sm:hover:border-gray-300
               focus:text-tertiary whitespace-no-wrap sm:focus:border-tertiary">
